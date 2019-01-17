6a7aaf0dae3f276f882ca100c724557c0c2a136a
vv13 <zwhvv13@gmail.com>
2019-01-18 01:56:40 +0800

    fix: scroll-reveal

--- packages/vue-scroll-reveal/dist/vue-scroll-reveal.js
+++ packages/vue-scroll-reveal/dist/vue-scroll-reveal.js
@@ -5,77 +5,33 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-require("core-js/modules/es7.symbol.async-iterator");
+var _scrollreveal = _interopRequireDefault(require("scrollreveal"));
 
-require("core-js/modules/es6.symbol");
-
-require("core-js/modules/web.dom.iterable");
-
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
-
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
-
-function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-
-const sr = require('scrollreveal').default();
-
-function generateOptions(defaultOptions, bindingValue, bindingModifiers) {
-  const options = Object.assign({}, defaultOptions, bindingValue);
-
-  if (bindingModifiers) {
-    if (bindingModifiers.reset) {
-      options.reset = true;
-    }
-
-    if (bindingModifiers.nomobile) {
-      options.mobile = false;
-    }
-
-    if (bindingModifiers.nodesktop) {
-      options.desktop = false;
-    }
-  }
-
-  return options;
-}
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const VueScrollReveal = {
   install(Vue, defaultOptions) {
     Vue.directive('scroll-reveal', {
       inserted: (el, binding) => {
-        const options = generateOptions(defaultOptions, binding.value, binding.modifiers);
+        const options = Object.assign({}, defaultOptions, binding.value);
 
         if (typeof options.class === 'string') {
           el.classList.add(options.class);
           delete options.class;
         }
 
-        sr.reveal(el, options);
+        (0, _scrollreveal.default)().reveal(el, options);
       },
       update: (el, binding) => {
         if (binding.value != binding.oldValue) {
-          const options = generateOptions(defaultOptions, binding.value, binding.modifiers);
-          sr.reveal(el, options);
+          const options = Object.assign({}, defaultOptions, binding.value);
+          (0, _scrollreveal.default)().reveal(el, options);
         }
       }
     });
     Object.defineProperty(Vue.prototype, '$sr', {
       get() {
-        return Object.assign({}, sr, {
-          reveal: new Proxy(sr.reveal, {
-            apply: (target, ctx, args) => {
-              const _args = _slicedToArray(args, 4),
-                    el = _args[0],
-                    config = _args[1],
-                    interval = _args[2],
-                    sync = _args[3];
-
-              target.apply(ctx, [el, generateOptions(defaultOptions, config), interval, sync]);
-            }
-          })
-        });
+        return (0, _scrollreveal.default)();
       }
 
     });
